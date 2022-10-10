2f1cbeb558dea6b3a69758826aacc01b4c9ee7c8
Wellington-Silva <wellingtonsilva112000@gmail.com>
2022-10-10 16:35:49 -0300

    First commit hahaha

---
+++ helpdesk.js
@@ -0,0 +1,9 @@
+const x = require('./data.json');
+
+/* Questão 1 
+Quais são os chamados em aberto, identificando: número do chamado, data, filial chamadora, número identificador 
+do equipamento envolvido e problema informado pela filial;
+*/
+function chamados() {
+    console.log(x.user.name)
+}
