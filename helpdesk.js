741c5e92837db838586c5b700e944810915155a9
Wellington-Silva <wellingtonsilva112000@gmail.com>
2022-10-10 16:47:18 -0300

    fix bugs

--- helpdesk.js
+++ helpdesk.js
@@ -7,3 +7,27 @@ do equipamento envolvido e problema informado pela filial;
 function chamados() {
     console.log(x.user.name)
 }
+
+/* Questão 2 
+Quais são os equipamentos existentes em cada filial, indicando para cada filial:
+endereço da filial e seus equipamentos (número do equipamento, tipo de equipamento
+e data de instalação na filial;
+*/
+function equipamentos_existentes() {
+    console.log(x.equipamento)
+}
+
+
+/* Questão 3 
+Quais são os chamados com status de encerrado, indicando: número de cada chamado,
+data de abertura e data de conclusão do mesmo;
+*/
+
+/* Questão 4 
+Quais foram os técnicos do HelpDesk que mais atenderam chamados em cada mês do
+ano corrente (nome do técnico e quantidade de chamados atendidos com conclusão);
+*/
+
+/* Questão 5 
+Dado um determinado chamado, qual a solução dada para o mesmo.
+ */
