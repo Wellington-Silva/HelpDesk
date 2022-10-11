e10eca15dbd17af03afe29403e051214f4dd7435
Wellington-Silva <wellingtonsilva112000@gmail.com>
2022-10-11 08:52:37 -0300

    fix bugs

--- src/app.js
+++ src/app.js
@@ -12,29 +12,44 @@ app.get('/test', (req, res) => res.json(filiais));
 Quais são os chamados em aberto, identificando: número do chamado, data, filial chamadora, número identificador 
 do equipamento envolvido e problema informado pela filial;
 */
-function chamado() {
-    console.log(x.user.name)
-}
+app.get('/q1', (req, res) => {
+    const result = chamados.filter(chamado => chamado.status.toLocaleLowerCase() === "aberto")
+    const data = result.map(r => {
+        return {
+            numero: r._id,
+            data: r.dt_start,
+            filial: r.filial_id,
+            equipamento: r.equipment_id,
+            problema: r.problem
+        }
+    })
+    res.status(200).json(data)
+});
 
 /* Questão 2 
 Quais são os equipamentos existentes em cada filial, indicando para cada filial:
 endereço da filial e seus equipamentos (número do equipamento, tipo de equipamento
 e data de instalação na filial;
 */
-function equipamentos_existentes() {
-    console.log(x.equipamento)
-}
+app.get('/q2', (req, res) => {
 
+});
 
 /* Questão 3 
 Quais são os chamados com status de encerrado, indicando: número de cada chamado,
 data de abertura e data de conclusão do mesmo;
 */
+app.get('/q3', (req, res) => {
+    const result = chamados.filter(chamado => chamado.status.toLocaleLowerCase() === "fechado")
+    const data = result.map(r => { return { numero: r._id, abertura: r.dt_start, conclusao: r.dt_end } })
+    res.status(200).json(data)
+});
 
 /* Questão 4 
 Quais foram os técnicos do HelpDesk que mais atenderam chamados em cada mês do
 ano corrente (nome do técnico e quantidade de chamados atendidos com conclusão);
 */
+app.get('/q4', (req, res) => { });
 
 /* Questão 5 
 Dado um determinado chamado, qual a solução dada para o mesmo.
