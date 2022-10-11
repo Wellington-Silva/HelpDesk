602eea9cd3a6853ba3d6837aef49f1d21822de35
Caio <Caio.almeida16.ca@gmail.com>
2022-10-11 09:40:35 -0300

    q2

--- src/app.js
+++ src/app.js
@@ -13,17 +13,17 @@ Quais são os chamados em aberto, identificando: número do chamado, data, filia
 do equipamento envolvido e problema informado pela filial;
 */
 app.get('/q1', (req, res) => {
-    const result = chamados.filter(chamado => chamado.status.toLocaleLowerCase() === "aberto")
-    const data = result.map(r => {
-        return {
-            numero: r._id,
-            data: r.dt_start,
-            filial: r.filial_id,
-            equipamento: r.equipment_id,
-            problema: r.problem
-        }
-    })
-    res.status(200).json(data)
+  const result = chamados.filter(chamado => chamado.status.toLocaleLowerCase() === "aberto")
+  const data = result.map(r => {
+    return {
+      numero: r._id,
+      data: r.dt_start,
+      filial: r.filial_id,
+      equipamento: r.equipment_id,
+      problema: r.problem
+    }
+  })
+  res.status(200).json(data)
 });
 
 /* Questão 2 
@@ -32,7 +32,16 @@ endereço da filial e seus equipamentos (número do equipamento, tipo de equipam
 e data de instalação na filial;
 */
 app.get('/q2', (req, res) => {
-
+  let equips;
+  const result = filiais.map(filial => {
+    equips = equipamentos.filter(e => e.filial_id === filial._id);
+    return {
+      filial: filial.name,
+      endereco: filial.address,
+      equipamentos: equips
+    };
+  });
+  res.status(200).json(result);
 });
 
 /* Questão 3 
@@ -40,21 +49,22 @@ Quais são os chamados com status de encerrado, indicando: número de cada chama
 data de abertura e data de conclusão do mesmo;
 */
 app.get('/q3', (req, res) => {
-    const result = chamados.filter(chamado => chamado.status.toLocaleLowerCase() === "fechado")
-    const data = result.map(r => { return { numero: r._id, abertura: r.dt_start, conclusao: r.dt_end } })
-    res.status(200).json(data)
+  const result = chamados.filter(chamado => chamado.status.toLocaleLowerCase() === "fechado")
+  const data = result.map(r => { return { numero: r._id, abertura: r.dt_start, conclusao: r.dt_end } })
+  res.status(200).json(data)
 });
 
 /* Questão 4 
 Quais foram os técnicos do HelpDesk que mais atenderam chamados em cada mês do
 ano corrente (nome do técnico e quantidade de chamados atendidos com conclusão);
 */
-app.get('/q4', (req, res) => { });
+app.get('/q4', (req, res) => {
+
+});
 
 /* Questão 5 
 Dado um determinado chamado, qual a solução dada para o mesmo.
 */
-
 app.get('/q5/:idchamado', (req, res) => {
   const { idchamado } = req.params;
   const chamado = chamados.find(chamado => chamado._id === parseInt(idchamado));
